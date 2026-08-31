f21c292df1979c44474526ad2acc64278e19d6b9
Abdulrahman M. Salem <anon.rt01@gmail.com>
2026-08-31 09:15:53 +0300

    add better-auth

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { db } from "@/drizzle";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { betterAuth } from "better-auth";
+import { username } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  baseURL:
+    process.env.BETTER_AUTH_URL ?? process.env.VERCEL_URL,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: { enabled: true },
+  plugins: [username({ displayUsername: false })],
+});
